1c9407dd72a2fd3e2a9a15d74504c727b82580db
Stilone <54247765+Stilone@users.noreply.github.com>
2021-05-23 15:29:08 +0300

    setTimeout
    13

--- setTimeout.js
+++ setTimeout.js
@@ -1,16 +1,17 @@
 const btnElement = document.getElementById('btn');
-console.log(btnElement);
 
-// const showColor = () => {
-//         const classes = btnElement.classList;
-//         const result = classes.contains('btn-y');
-//
-//         if (result) {
-//             btnElement.classList.remove('btn-y');
-//             btnElement.classList.add('btn-b');
-//         } else {
-//             btnElement.classList.add('btn-y');
-//         }
-// }
-//
-// setInterval(showColor, 5000)
+const showColor = () => {
+    if(btnElement) {
+        const classes = btnElement.classList;
+        const result = classes.contains('btn-y');
+
+        if (result) {
+            btnElement.classList.remove('btn-y');
+            btnElement.classList.add('btn-b');
+        } else {
+            btnElement.classList.add('btn-y');
+        }
+    }
+}
+
+setInterval(showColor, 5000)
